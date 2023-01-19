c44345391a90f02382fb86ddfdfcd606ed9f8704
Tim Smith <tim.smith2814@gmail.com>
2023-01-19 10:04:16 +0000

    ADd class 06 demo

---
+++ class-06/demo/app.js
@@ -0,0 +1,18 @@
+let myName = prompt("What is your name?");
+
+let time = prompt("What hour of the day is it? (0-23)");
+let message;
+
+if (time <= 11) {
+  message = "Good morning " + myName + "!";
+} else if (time <= 18) {
+  message = "Good afternoon " + myName + "!";
+} else if (time < 24) {
+  message = "Good evening " + myName + "!";
+} else {
+  message = "C'mon " + myName + ", that is not a valid time!";
+}
+
+console.log(message);
+
+document.write(message);
