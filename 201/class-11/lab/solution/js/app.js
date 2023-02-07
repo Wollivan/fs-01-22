2a0d3b704ce1823b4cd8bdd39c81b174710f06ed
Tim Smith <tim.smith2814@gmail.com>
2023-02-07 10:09:52 +0000

    lab 11 solution start

---
+++ 201/class-11/lab/solution/js/app.js
@@ -0,0 +1,42 @@
+function getRandomNumber() {
+  return Math.floor(Math.random() * Product.allProducts.length);
+}
+
+function Product(name, src) {
+  this.name = name;
+  this.src = src;
+  this.clicks = 0;
+  this.views = 0;
+  Product.allProducts.push(this);
+}
+
+Product.allProducts = [];
+
+new Product("Bag", "assets/bag.jpeg");
+
+const productNames = [
+  "bag",
+  "banana",
+  "bathroom",
+  "boots",
+  "breakfast",
+  "bubblegum",
+  "chair",
+  "cthulhu",
+  "dog-duck",
+  "dragon",
+  "pen",
+  "pet-sweep",
+  "scissors",
+  "shark",
+  "tauntaun",
+  "unicorn",
+  "water-can",
+  "wine-glass",
+];
+
+for (let i = 0; i < productNames.length; i++) {
+  new Product(productNames[i], `assets/${productNames[i]}.jpeg`);
+}
+
+// render our products onto the page
