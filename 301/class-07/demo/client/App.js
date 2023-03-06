ded5adc9dd4cc09e51599066dc8fd41a4e22f045
Wollivan <tim.smith2814@gmail.com>
2023-03-06 21:18:11 +0000

    Add class 7 demo

---
+++ 301/class-07/demo/client/App.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
+
+function App() {
+  const [list, setList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // function to retrieve shopping list data
+  async function getShoppingList() {
+    const API = `http://localhost:8080?type=${searchQuery}`;
+    const res = await axios.get(API);
+
+    setLocation(res.data);
+  }
+
+  function handleSearch(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <input onChange={handleSearch} placeholder="Search for a type of item" />
+      <button onClick={getShoppingList}>Get Data</button>
+      {/* map through our results */}
+      {list.map((item) => {
+        return (
+          <div>
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
