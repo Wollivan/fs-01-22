a85d7998bea6aa82ca5f3df503488a89e977264b
Tim Smith <tim.smith2814@gmail.com>
2023-03-14 11:41:14 +0000

    Create delete demo

--- 301/class-12/demo/client/src/App.js
+++ 301/class-12/demo/client/src/App.js
@@ -5,6 +5,12 @@ import { useEffect, useState } from "react";
 function App() {
   const [cats, setCats] = useState([]);
   const [location, setLocation] = useState("");
+  const [form, setForm] = useState({
+    name: "",
+    color: "",
+    hasClaws: false,
+    location: "",
+  });
 
   useEffect(() => {
     getCats();
@@ -17,11 +23,45 @@ function App() {
       API = API + "?location=" + location;
     }
     const res = await axios.get(API);
+    console.log(res.data);
     setCats(res.data);
   }
 
   function handleLocation(event) {
     setLocation(event.target.value);
+  }
+
+  function handleChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+    console.log(form);
+  }
+
+  async function handleAddCat(event) {
+    event.preventDefault();
+    const API = "http://localhost:8080/cats";
+    const res = await axios.post(API, form);
+
+    // add our new cat to the page
+    const newCatsList = [...cats, res.data];
+    setCats(newCatsList);
+
+    // reset the form
+    setForm({
+      name: "",
+      color: "",
+      hasClaws: false,
+      location: "",
+    });
+  }
+
+  async function deleteCat(id, name) {
+    const confirmDelete = window.confirm(`Are you sure you want to permantently delete ${name}?`);
+    if (confirmDelete) {
+      const API = `http://localhost:8080/cats/${id}`;
+      const res = await axios.delete(API);
+      console.log(res);
+      getCats();
+    }
   }
 
   return (
@@ -33,9 +73,17 @@ function App() {
           <div key={index}>
             <h3>{cat.name}</h3>
             <p>{cat.location}</p>
+            <span onClick={() => deleteCat(cat._id, cat.name)}>X</span>
           </div>
         );
       })}
+      <h2>Add a Cat</h2>
+      <form onSubmit={handleAddCat}>
+        <input name="name" value={form.name} onChange={handleChange} placeholder="Name" />
+        <input name="color" value={form.color} onChange={handleChange} placeholder="Color" />
+        <input name="location" value={form.location} onChange={handleChange} placeholder="Location" />
+        <button type="submit">Add Cat</button>
+      </form>
     </div>
   );
 }
